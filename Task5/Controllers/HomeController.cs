5d72397f09c0e54ee15212e8cd1f6f0ffca3b69b
Виталий Лозюк <vitalik050405@gmail.com>
2023-08-09 22:32:56 +0300

    Added saving to CSV file

--- Task5/Controllers/HomeController.cs
+++ Task5/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using Task5.Models;
 using Task5.Utils;
 
@@ -18,6 +19,26 @@ namespace Task5.Controllers
             FakeDataGenerator generator = new(model);
             var fakeData = generator.GenerateUsersData();
             return PartialView("_UsersData", fakeData);
+        }
+
+        [HttpPost("getCsvFile")]
+        public IActionResult GetCsvFile(GeneratorConfigurationModel model)
+        {
+            var allPagesData = generateAllPages(model, model.Page);
+            var csv = new CsvConverter<FakeUserDataModel>(allPagesData).GetCsvString();
+            return File(Encoding.UTF8.GetBytes(csv.ToCharArray()), "text/csv", "FakeData.csv");
+        }
+
+        private List<FakeUserDataModel> generateAllPages(GeneratorConfigurationModel model, int pages)
+        {
+            var allPagesData = new List<FakeUserDataModel>();
+            for (int i = 0; i <= pages; i++)
+            {
+                model.Page = i;
+                FakeDataGenerator generator = new(model);
+                allPagesData.AddRange(generator.GenerateUsersData());
+            }
+            return allPagesData;
         }
     }
 }
---
+++ Task5/Utils/CsvConverter.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using Task5.Models;
+
+namespace Task5.Utils
+{
+    public class CsvConverter<T>
+    {
+        private readonly List<T> records;
+
+        private readonly CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ",",
+            HasHeaderRecord = true
+        };
+
+        public CsvConverter(List<T> records)
+        {
+            this.records = records;
+        }
+
+        public string GetCsvString()
+        {
+            using (var stringWriter = new StringWriter())
+            using (var csv = new CsvWriter(stringWriter, csvConfig))
+            {
+                csv.WriteRecords(records);
+                return stringWriter.ToString();
+            }
+        }
+    }
+}
